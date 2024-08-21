88e27e30d785c2a02772f96a56445f068bcc3b36
Ethiel <ethiel97@gmail.com>
2024-08-21 14:29:16 +0000

    Update middlewares.js

--- config/middlewares.js
+++ config/middlewares.js
@@ -3,15 +3,15 @@ module.exports = [
   'strapi::errors',
   'strapi::security',
   'strapi::poweredBy',
-  /*{
+  {
     name: 'strapi::cors',
     config: {
       enabled: false,
-        origin: '*',
+        origin: ['http://localhost:3000','http://localhost:3001','https://fluttergigs.com', 'fluttergigs.com'],
         headers: '*',
-        // credentials: false,
+        credentials: true,
     },
-  },*/
+  },
   'strapi::query',
   'strapi::body',
   'strapi::session',
