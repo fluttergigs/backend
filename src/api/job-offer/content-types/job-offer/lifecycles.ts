f11c4074debad883e8d20217d68698c4cb775ce4
Ethiel <ethiel97@gmail.com>
2025-04-28 21:31:30 -0400

    feat(jobs): Add Twitter integration for automated job posting
    Introduce `TwitterWrapper` to handle Twitter API interactions, enabling automated job post tweets. Added lifecycle hook in the job-offer content type to trigger a tweet after job creation, along with utilities for formatting job details and location.

---
+++ src/api/job-offer/content-types/job-offer/lifecycles.ts
@@ -0,0 +1,21 @@
+import {TwitterWrapperImpl} from "../../../../services/twitter_api";
+
+
+export default {
+  async afterCreate(event: any) {
+    try {
+      const {result} = event;
+
+      const twitterService = new TwitterWrapperImpl();
+
+      await twitterService.createTweet({
+        text: twitterService.buildJobTweet(result)
+      });
+
+    } catch (err) {
+
+      //@ts-ignore
+      strapi.log.error('Failed to send new posted job tweet:', err);
+    }
+  }
+};
---
+++ src/services/twitter_api.ts
@@ -0,0 +1,82 @@
+import {TwitterApi} from "twitter-api-v2";
+import {JobFormatter} from "../../helpers/JobFormatter";
+
+export interface Tweet {
+  text: string;
+
+  [key: string]: any;
+}
+
+
+const appKey: string = process.env.TWITTER_APP_TOKEN;
+const appSecret: string = process.env.TWITTER_APP_SECRET;
+const accessToken: string = process.env.TWITTER_ACCESS_TOKEN;
+const accessSecret: string = process.env.TWITTER_ACCESS_SECRET;
+
+const twitterClient = new TwitterApi({
+    accessToken,
+    appSecret,
+    appKey,
+    accessSecret
+  }
+);
+
+abstract class TwitterWrapper {
+  abstract createTweet<T extends Tweet>(data: T): Promise<void>;
+
+
+   buildJobTweet<T extends {
+    companyName?: string;
+    title?: string;
+    location?: string;
+    salaryRange?: string;
+    howToApply?: string
+    salaryFrom?: string;
+    salaryTo?: string
+    slug?: string
+    workPermits: []
+    company: {
+      name: string,
+    },
+    remoteOptions: string,
+  }>(data: T): string {
+    const companyName = data.company.name ?? 'GarbusCorp';
+    const jobTitle = data.title ?? 'Flutter Developer';
+    const salaryRange = data.salaryFrom ? `💵 Salary: $${data.salaryFrom} - ${data.salaryTo}` : 'N/A'
+    const applicationLink = `https://fluttergigs.com/jobs/${data.slug ?? ''}`
+    const remote = JobFormatter.formatRemoteOption(data.remoteOptions);
+    const location = JobFormatter.formatWorkPermits(data.workPermits);
+
+    return `
+🚀 Hiring Flutter Devs! 🚀
+
+🏢 Company: ${companyName}
+💼 Role: ${jobTitle}
+🌍 Location: ${location}
+🏡 Remote: ${remote}
+
+${salaryRange}
+
+Apply now 👇
+${applicationLink}
+
+#FlutterJobs #Hiring #RemoteJobs
+    `.trim();
+  }
+}
+
+
+export class TwitterWrapperImpl extends TwitterWrapper {
+  async createTweet<T extends Tweet>(data: T) {
+
+    try {
+      const {data: createdTweet} = await twitterClient.v2.tweet(data.text);
+      console.log('Tweet', createdTweet.id, ':', createdTweet.text);
+    } catch (e) {
+
+      console.error('Error creating tweet:', e);
+    }
+  }
+
+
+}
